85b2d697b12f1b653e6f3b35a0c28b63d9ae5fba
Axel <axel.demorest@gmail.com>
2020-11-20 22:07:35 +0100

    begin

---
+++ php/home.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/home.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>La patte d'alex</title>
+</head>
+
+<body>
+    <div class="navbar">
+        <div class="navbar-left">
+            <img class="navbar-logo" src="../img/cat.png" width="8%">
+            <ul>
+                <li><a href="#">Accueil</a></li>
+                <li><a href="#">Présentation</a></li>
+                <li><a href="#">Produits</a></li>
+            </ul>
+        </div>
+
+        <div class="navbar-right">
+            <ul>
+                <li><a class="link-inscription" href="#">S'inscire</a></li>
+                <li><a class="link-connect" href="#">Se connecter</a></li>
+            </ul>
+        </div>
+    </div>
+
+    <header class="header">
+        <img src="../img/banner.jpg" width="100%">
+    </header>
+</body>
+
+</html>
